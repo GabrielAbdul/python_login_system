929f6921cab25e6d22a13f5564141aa4600df3d9
Gabriel <gabrielabdul1@gmail.com>
2020-06-11 01:30:18 +0000

    input verification, readline() -> readlines()

--- login.py
+++ login.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-"""A python user login system"""
+"""Login Module"""
 
 
 import json
 
 def login():
-	"""Function that will parse file containing user data and verify login credentials"""
-	user_name = input("Username: ")
-	user_pass = input("Password: ")
+    """Function that will parse file containing user data and verify login credentials"""
+    user_name = input("Username: ")
+    if user_name == "":
+        print("Must enter user name")
+        return
+    user_pass = input("Password: ")
+    if user_pass == "":
+        print("Must enter password")
+        return
 
-	try:
-		with open("credentials.txt", 'r') as credentials:
-			user_cred = json.loads(credentials.readline())
-			if user_cred['user_name'] == user_name:
-				if user_cred['user_password'] == user_pass:
-						print("SUCCESS")
-				elif user_cred['user_password'] != user_pass:
-					print("INVALID PASS")
-			if user_cred['user_name'] != user_name:
-				print("invalid user name")
-	except FileNotFoundError:
-		print("File Not Found")
-
+    try:
+        with open("credentials.txt", 'r') as credentials:
+            lines = credentials.readlines()
+            for line in lines:
+                user_cred = json.loads(line)
+                if user_cred['user_name'] == user_name:
+                    if user_cred['user_password'] == user_pass:
+                        print("SUCCESS")
+                        break
+                    elif user_cred['user_password'] != user_pass:
+                        print("INVALID PASS")
+                if user_cred['user_name'] != user_name:
+                    print("invalid user name")
+    except FileNotFoundError:
+        print("File Not Found")
