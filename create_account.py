e0fc34739ceafae6491ac0dc1c0e073164edcb01
Gabriel <gabrielabdul1@gmail.com>
2020-06-11 01:31:11 +0000

    added sort_keys=True, to JSON deserialization

--- create_account.py
+++ create_account.py
@@ -7,20 +7,20 @@ import json
 
 
 def create_account():
-	"""Function to create an account"""
-	user_email = input("What is your email?\n")
-	user_name = input("What is your user name?\n")
-	user_password = input("What is your password?\n")
+    """Function to create an account"""
+    user_email = input("What is your email?\n")
+    user_name = input("What is your user name?\n")
+    user_password = input("What is your password?\n")
 
-	user_info = {
-		"user_email": user_email,
-		"user_name": user_name,
-		"user_password": user_password
-	}
+    user_info = {
+    "user_email": user_email,
+    "user_name": user_name,
+    "user_password": user_password
+    }
 
-	try:
-		with open("credentials.txt", 'x') as credentials:
-			credentials.write("{}\n".format(json.dumps(user_info)))
-	except FileExistsError:
-		with open("credentials.txt", 'a') as credentials:
-			credentials.write("{}\n".format(json.dumps(user_info)))
+    try:
+        with open("credentials.txt", 'x') as credentials:
+            credentials.write("{}\n".format(json.dumps(user_info, sort_keys=True)))
+    except FileExistsError:
+        with open("credentials.txt", 'a') as credentials:
+            credentials.write("{}\n".format(json.dumps(user_info, sort_keys=True)))
